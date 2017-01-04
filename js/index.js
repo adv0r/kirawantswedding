b705d0bec36077d5983bc2360b8b77c219f47fbc
Daniele <dnlml@users.noreply.github.com>
2017-01-04 13:25:41 +0100

    Optimized

--- js/index.js
+++ js/index.js
@@ -2,16 +2,27 @@
 
 var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy){
   // Initialise core variables
-  var r = $(pupil).width()/2;
+  var $pupil = $(pupil);
+  var xp = 0, yp = 0;
+  var r = $pupil.width()/2;
+  var $eyecontainer = $(eyecontainer);
   var center = {
-    x: $(eyecontainer).width()/2 - r, 
-    y: $(eyecontainer).height()/2 - r
+    x: $eyecontainer.width()/2 - r,
+    y: $eyecontainer.height()/2 - r
   };
-  var distanceThreshold = $(eyecontainer).width()/2 - r;
+  var distanceThreshold = $eyecontainer.width()/2 - r;
   var mouseX = 0, mouseY = 0;
-  
+
+  $(window).on('resize', function () {
+    r = $pupil.width()/2;
+    center = {
+      x: $eyecontainer.width()/2 - r,
+      y: $eyecontainer.height()/2 - r
+    };
+    distanceThreshold = $eyecontainer.width()/2 - r;
+  });
   // Listen for mouse movement
-  $(window).mousemove(function(e){ 
+  $(window).mousemove(function(e){
     var d = {
       x: e.pageX - r - eyeposx - center.x,
       y: e.pageY - r - eyeposy - center.y
@@ -25,15 +36,13 @@ var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy){
       mouseY = d.y / distance * distanceThreshold + center.y;
     }
   });
-  
+
   // Update pupil location
-  var pupil = $(pupil);
-  var xp = 0, yp = 0;
   var loop = setInterval(function(){
     // change 1 to alter damping/momentum - higher is slower
     xp += (mouseX - xp) / 1;
     yp += (mouseY - yp) / 1;
-    pupil.css({left:xp, top:yp});    
+    $pupil.css({left:xp, top:yp});
   }, 1);
 };
 
