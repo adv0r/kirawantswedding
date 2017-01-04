bceb15d892e46d31c7ea4668f350d4d573f3efcd
Daniele <dnlml@users.noreply.github.com>
2017-01-04 20:36:00 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ var DrawEye = function(eyecontainer, pupil, eyeposx, eyeposy){
   var xp = 0, yp = 0;
   var r = $pupil.width()/2;
   var $eyecontainer = $(eyecontainer);
+  console.log($eyecontainer);
+  
   var center = {
     x: $eyecontainer.width()/2 - r,
     y: $eyecontainer.height()/2 - r
