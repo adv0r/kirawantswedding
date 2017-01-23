cbc1f229a1e9b15647784fcead7f7e28280ce4bc
Nicolò Paternoster <hi@adva.io>
2017-01-23 17:26:13 +0100

    aggiunge classe

--- js/index.js
+++ js/index.js
@@ -24,17 +24,18 @@ $( document ).ready(function() {
         var invitees = codeTable[this.value.toUpperCase()][0];
         console.log("Welcome "+invitees);
         
-        $('#sunbtn').prop('disabled', false);
-
-        $("#sunbtn").toggle("highlight");
-        setTimeout(function(){ $("#sunbtn" ).toggle("highlight"); } , 400);
+        //$('#sunbtn').prop('disabled', false);
+        $('#sunbtn').addClass('glow-please'); 
+        //$("#sunbtn").toggle("highlight");
+        //setTimeout(function(){ $("#sunbtn" ).toggle("highlight"); } , 400);
 
         //Text to speech
         //var msg = new SpeechSynthesisUtterance("Welcome "+invitees);
         //window.speechSynthesis.speak(msg);
     }
     else {
-        $('#sunbtn').prop('disabled', true);
+        //$('#sunbtn').prop('disabled', true);
+        $('#sunbtn').removeClass('glow-please'); 
     }
   };
 });
