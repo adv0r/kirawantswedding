01fbdc07cc0c51894714b8ff281927c44cd4cb0c
Nicolò Paternoster <hi@adva.io>
2017-02-16 12:00:33 +0100

    newline

--- js/nda.js
+++ js/nda.js
@@ -2,4 +2,5 @@ var url = window.location.href;
 var captured = /n=([^&]+)/.exec(url)[1]; 
 var result = captured ? captured : 'n';
 result = str.replace('%20',' ');
+
 document.getElementById("nomespan").innerHTML = result;
