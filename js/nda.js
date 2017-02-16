21b95fc3b6618d9ad6bbf90897e7c50a8ff557a5
Nicolò Paternoster <hi@adva.io>
2017-02-16 11:45:43 +0100

    url missing

--- js/nda.js
+++ js/nda.js
@@ -1,3 +1,4 @@
+var url = window.location.href;
 var captured = /n=([^&]+)/.exec(url)[1]; 
 var result = captured ? captured : 'n';
 document.getElementById("nomespan").innerHTML = "Paragraph changed!";
